458549fbc9e181581c428c1dace68ea2ea6c44fb
Houssem Dodor <houssem@dodor.nl>
2024-05-04 12:15:48 +0200

    added validation & hashing for signup

--- mern-stack-app/backend/models/userModel.js
+++ mern-stack-app/backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
@@ -19,19 +20,26 @@ const userSchema = new Schema(
 );
 
 // Static signup method
-userSchema.statics.signup = async function(email, password) {
-  const exists = await this.findOne({ email });
+userSchema.statics.signup = async function (email, password) {
+  //Check if fields are empty
+  if (!email || !password) throw Error("All fields must be filled");
 
-  if (exists) {
-    throw Error("Email already in use");
-  }
+  //Check if email is a valid one
+  if (!validator.isEmail(email)) throw Error("Email is not valid");
+
+  //Check password
+  if (!validator.isStrongPassword(password)) throw Error("Password is not strong enough");
+
+  // CHeck if the email is already in use
+  const exists = await this.findOne({ email });
+  if (exists) throw Error("Email already in use");
 
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
 
   const user = await this.create({ email, password: hash });
 
-  return user
+  return user;
 };
 
 module.exports = mongoose.model("User", userSchema);
