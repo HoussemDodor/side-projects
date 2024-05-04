aa8319982645e6f65858ead31a4ba8ffe1c51a27
Houssem Dodor <houssem@dodor.nl>
2024-05-04 11:50:34 +0200

    added signup functionality

--- mern-stack-app/backend/controllers/userController.js
+++ mern-stack-app/backend/controllers/userController.js
@@ -1,14 +1,21 @@
-const User = require('../models/userModel')
+const User = require("../models/userModel");
 
-// Login 
+// Login
 const loginUser = async (req, res) => {
-    res.json({message: 'User login'})
-}
+  res.json({ message: "User login" });
+};
 
 // Signup
 const signupUser = async (req, res) => {
-    res.json({message: 'User signup'})
+  const { email, password } = req.body;
 
-}
+  try {
+    const user = await User.signup(email, password);
 
-module.exports = { loginUser, signupUser }
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = { loginUser, signupUser };
