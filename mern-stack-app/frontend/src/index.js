a3b0d754d28b1d456f57dd496abe224c83959d04
Houssem Dodor <houssem@dodor.nl>
2024-05-04 12:52:44 +0200

    added authcontext

--- mern-stack-app/frontend/src/index.js
+++ mern-stack-app/frontend/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { WorkoutContextProvider } from "./context/workoutContext";
+import { AuthContext } from "./context/authContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <WorkoutContextProvider>
-      <App />
-    </WorkoutContextProvider>
+    <AuthContext.Provider>
+      <WorkoutContextProvider>
+        <App />
+      </WorkoutContextProvider>
+    </AuthContext.Provider>
   </React.StrictMode>
 );
