65a058e51bedf5d5719de7fbaacd96c37137a2da
Houssem Dodor <houssem@dodor.nl>
2024-06-05 13:24:13 +0200

    added error handling

--- dodor-app/frontend/src/pages/CustomerDetails.js
+++ dodor-app/frontend/src/pages/CustomerDetails.js
@@ -11,11 +11,7 @@ const CustomerDetails = () => {
   const { id } = useParams();
 
   const [error, setError] = useState("");
-  const [name, setName] = useState("");
-  const [surname, setSurname] = useState("");
-  const [email, setEmail] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [createdAt, setCreatedAt] = useState("");
+  const [customer, setCustomer] = useState(null);
 
   useEffect(() => {
     axios
@@ -23,12 +19,7 @@ const CustomerDetails = () => {
         headers: { Authorization: `Bearer ${user.acces_token}` },
       })
       .then((res) => {
-        console.log(res.data);
-        setName(res.data.name);
-        setSurname(res.data.surname);
-        setEmail(res.data.email);
-        setPhoneNumber(res.data.phoneNumber);
-        setCreatedAt(res.data.createdAt);
+        setCustomer(res.data);
       })
       .catch((err) => {
         setError(
@@ -39,6 +30,7 @@ const CustomerDetails = () => {
 
   const handleSave = (e) => {
     e.preventDefault();
+    console.log(customer);
 
     console.log("HandleSave method not yet implemented");
   };
@@ -49,113 +41,148 @@ const CustomerDetails = () => {
 
   return (
     <div className="bg-gray-200 min-h-[93vh] flex justify-center">
-      <form
-        onSubmit={handleSave}
-        className="bg-gray-100 rounded shadow-lg max-h-[90vh] my-5 p-5 overflow-y-scroll"
-      >
-        <Link
+      {customer ? (
+        <form
+          onSubmit={handleSave}
+          className="bg-gray-100 rounded shadow-lg max-h-[90vh] my-5 p-5 overflow-y-scroll"
+        >
+          <Link
+            to="/customerOverview"
+            className="text-white inline-flex mb-5 bg-gray-800 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg w-auto px-4 py-2.5 text-center"
+          >
+            <ArrowLeftIcon className="size-5 mr-2 text-white" />
+            <button>Terug</button>
+          </Link>
+
+          <div className="mb-5">
+            <h1 className="text-3xl font-bold">
+              {customer.name + " " + customer.surname}
+            </h1>
+            <p className="italic">
+              {formatDistanceToNow(customer.createdAt, { locale: nl })} geleden
+              gecreërd
+            </p>
+            <p className="italic mb-5">
+              {formatDistanceToNow(customer.createdAt, { locale: nl })} geleden
+              laatst bewerkt
+            </p>
+            <div className="flex mb-5">
+              <div>
+                <label htmlFor="name" className="mr-2">
+                  Naam:
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={customer.name}
+                  onChange={(e) =>
+                    setCustomer({ ...customer, name: e.target.value })
+                  }
+                  className="mr-5 bg-gray-50 border border-gray-300 text-gray-900 text-m rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
+                />
+              </div>
+              <div>
+                <label htmlFor="surname" className="mr-2">
+                  Achternaam:
+                </label>
+                <input
+                  type="text"
+                  id="surname"
+                  name="surname"
+                  value={customer.surname}
+                  onChange={(e) =>
+                    setCustomer({ ...customer, surname: e.target.value })
+                  }
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-m rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
+                />
+              </div>
+            </div>
+            <div className="mb-5">
+              <label htmlFor="email" className="mr-2">
+                Email:
+              </label>
+              <input
+                type="text"
+                id="email"
+                name="email"
+                value={customer.email}
+                onChange={(e) =>
+                  setCustomer({ ...customer, email: e.target.value })
+                }
+                className="mr-2 w-full bg-gray-50 border border-gray-300 text-gray-900 text-m rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
+              />
+            </div>
+            <div className="mb-5">
+              <label htmlFor="phoneNumber" className="mr-2">
+                Telefoon:
+              </label>
+              <input
+                type="text"
+                id="phoneNumber"
+                name="phoneNumber"
+                value={customer.phoneNumber}
+                onChange={(e) =>
+                  setCustomer({ ...customer, phoneNumber: e.target.value })
+                }
+                className="mr-2 w-full bg-gray-50 border border-gray-300 text-gray-900 text-m rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
+              />
+            </div>
+            <div>
+              <label htmlFor="status" className="mr-2">
+                Status:
+              </label>
+              <select
+                id="status"
+                name="status"
+                onChange={(e) =>
+                  setCustomer({ ...customer, status: e.target.value })
+                }
+                className="mr-2 w-full bg-gray-50 border border-gray-300 text-gray-900 text-m rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
+              >
+                <option>In afwachting</option>
+                <option>Geaccepteerd</option>
+              </select>
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="text-white inline-flex mr-2 bg-gray-800 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg w-auto px-4 py-2.5 text-center"
+            >
+              Opslaan
+            </button>
+            <button
+              type="button"
+              onClick={handleDelete}
+              className="text-white inline-flex mr-2 bg-red-800 hover:bg-rose-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg w-auto px-4 py-2.5 text-center"
+            >
+              Verwijderen
+            </button>
+          </div>
+
+          {error && (
+            <div className="bg-[#ffefef] p-2 max-w-80 border-solid border-2 border-red-600 rounded-xl">
+              {error}
+            </div>
+          )}
+        </form>
+      ) : (
+        <div className="bg-gray-100 rounded shadow-lg max-h-[90vh] w-full m-5 p-5">
+          <h1 className="text-4xl mb-5 font-bold">Klant niet gevonden</h1>
+          <div className="bg-[#ffefef] p-2 mb-5 max-w-80 border-solid border-2 border-red-600 rounded-xl">
+              {error}
+            </div>
+            <Link
           to="/customerOverview"
           className="text-white inline-flex mb-5 bg-gray-800 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg w-auto px-4 py-2.5 text-center"
         >
           <ArrowLeftIcon className="size-5 mr-2 text-white" />
           <button>Terug</button>
         </Link>
-
-        <div className="mb-5">
-          <h1 className="text-3xl font-bold">{name + " " + surname}</h1>
-          <p className="italic">{formatDistanceToNow(createdAt, {locale: nl})} geleden gecreërd</p>
-          <p className="italic mb-5">{formatDistanceToNow(createdAt, {locale: nl})} geleden laatst bewerkt</p>
-          <div className="flex mb-5">
-            <div>
-              <label htmlFor="name" className="mr-2">
-                Naam:
-              </label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                className="mr-5 bg-gray-50 border border-gray-300 text-gray-900 text-m rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
-              />
-            </div>
-            <div>
-              <label htmlFor="surname" className="mr-2">
-                Achternaam:
-              </label>
-              <input
-                type="text"
-                id="surname"
-                name="surname"
-                value={surname}
-                onChange={(e) => setSurname(e.target.value)}
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-m rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
-              />
-            </div>
-          </div>
-          <div className="mb-5">
-            <label htmlFor="email" className="mr-2">
-              Email:
-            </label>
-            <input
-              type="text"
-              id="email"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="mr-2 w-full bg-gray-50 border border-gray-300 text-gray-900 text-m rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
-            />
-          </div>
-          <div className="mb-5">
-            <label htmlFor="phoneNumber" className="mr-2">
-              Email:
-            </label>
-            <input
-              type="text"
-              id="phoneNumber"
-              name="phoneNumber"
-              value={phoneNumber}
-              onChange={(e) => setPhoneNumber(e.target.value)}
-              className="mr-2 w-full bg-gray-50 border border-gray-300 text-gray-900 text-m rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
-            />
-          </div>
-          <div>
-            <label htmlFor="status" className="mr-2">
-              Status:
-            </label>
-            <select
-              id="status"
-              name="status"
-              className="mr-2 w-full bg-gray-50 border border-gray-300 text-gray-900 text-m rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
-            >
-              <option>In afwachting</option>
-              <option>Geaccepteerd</option>
-            </select>
-          </div>
         </div>
-
-        <div>
-          <button
-            type="submit"
-            className="text-white inline-flex mr-2 bg-gray-800 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg w-auto px-4 py-2.5 text-center"
-          >
-            Opslaan
-          </button>
-          <button
-            type="button"
-            onClick={handleDelete}
-            className="text-white inline-flex mr-2 bg-red-800 hover:bg-rose-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg w-auto px-4 py-2.5 text-center"
-          >
-            Verwijderen
-          </button>
-        </div>
-
-        {error && (
-          <div className="bg-[#ffefef] p-2 max-w-80 border-solid border-2 border-red-600 rounded-xl">
-            {error}
-          </div>
-        )}
-      </form>
+      )}
     </div>
   );
 };
