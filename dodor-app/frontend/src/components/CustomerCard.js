06d1780d6dacef37221fa7339a61295e751afd81
Houssem Dodor <houssem@dodor.nl>
2024-06-05 16:44:41 +0200

    changed position of text

--- dodor-app/frontend/src/components/CustomerCard.js
+++ dodor-app/frontend/src/components/CustomerCard.js
@@ -9,8 +9,7 @@ const CustomerCard = (props) => {
         <div className="bg-slate-300 my-5 p-5 rounded-2xl shadow-md hover:bg-slate-500">
           <p className="font-bold">{props.customer.name} {props.customer.surname}</p>
           <p>{props.customer.email}</p>
-          <br/>
-          <p className="italic">{formatDistanceToNow(props.customer.createdAt, {locale: nl})} geleden</p>
+          <p className="italic text-right">{formatDistanceToNow(props.customer.createdAt, {locale: nl})} geleden</p>
         </div>
       </Link>
     </>
