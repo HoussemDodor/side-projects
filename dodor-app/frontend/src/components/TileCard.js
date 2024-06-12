ecf81bb423d7ca0aa675ba12ab84b5781e5f63d5
Houssem Dodor <houssem@dodor.nl>
2024-06-12 15:07:23 +0200

    tile overview page fully functional

--- dodor-app/frontend/src/components/TileCard.js
+++ dodor-app/frontend/src/components/TileCard.js
@@ -1,9 +1,69 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const TileCard = (props) => {
   return (
     <>
-      <div className="bg-slate-300 m-5 p-5 rounded-2xl w-1/2 md:w-1/4 h-[350px] shadow-md hover:bg-slate-500">{props.tile.title}</div>
+      <div className="bg-white text-gray-900 w-80 m-2 min-h-[10rem] shadow-lg rounded-md overflow-hidden">
+        <img
+          className="w-full h-auto max-h-5/6 object-cover"
+          alt="Tile"
+          src={props.tile.picture}
+        />
+        <div className="p-5 flex-col gap-3">
+          {/* Badge */}
+          <div className="flex items-center gap-2">
+            <span className="px-3 py-1 rounded full text-xs bg-gray-100">
+              {props.tile.size}
+            </span>
+            {props.tile.outdoors ? (
+              <span className="px-3 py-1 rounded full text-xs bg-gray-100">
+                Buiten
+              </span>
+            ) : null}
+            {props.tile.inStore ? (
+              <span className="px-3 py-1 rounded full text-xs bg-gray-100">
+                Winkel
+              </span>
+            ) : null}
+          </div>
+
+          <h2
+            title={props.tile.title}
+            className="font-semibold text-2xl overflow-ellipsis overflow-hidden whitespace-nowrap"
+          >
+            {props.tile.title}
+          </h2>
+
+          {/* Product Price */}
+          <div>
+            <span className="text-xl font-bold">
+              €{props.tile.price}/{props.tile.unit}
+            </span>
+            <div className="flex items-center gap-3 mt-1">
+              <span className="text-sm opacity-50">
+                €{props.tile.importPrice}/{props.tile.unit}
+              </span>
+            </div>
+          </div>
+
+          {/* Supplier */}
+          <span className="flex items-center mt-1 text-xs text-gray-500">
+            {props.tile.supplier}
+          </span>
+
+          {/* Product Action */}
+          <Link to={`/tile/${props.tile._id}`}>
+            <div className="flex justify-end gap-2">
+              <button
+                type="button"
+                className="bg-gray-800 hover:bg-blue-800 p-3 rounded-md text-white font-medium tracking-wider transition"
+              >
+                Bewerken
+              </button>
+            </div>
+          </Link>
+        </div>
+      </div>
     </>
   );
 };
--- dodor-app/frontend/src/pages/TileOverview.js
+++ dodor-app/frontend/src/pages/TileOverview.js
@@ -1,14 +1,16 @@
 import { useState, useEffect } from "react";
 import { useAuthContext } from "../hooks/useAuthContext";
 import axios from "../api/axios";
+import { PlusIcon } from "@heroicons/react/24/solid";
 
-import TileCard from "../components/TileCard"
+import TileCard from "../components/TileCard";
 
 const TileOverview = () => {
   const { user } = useAuthContext();
 
-  const [error, setError] = useState("");
   const [tiles, setTiles] = useState("");
+  const [searchResult, setSearchResult] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     axios
@@ -17,23 +19,88 @@ const TileOverview = () => {
       })
       .then((res) => {
         setTiles(res.data);
+        setSearchResult(res.data);
       })
       .catch((err) => {
-        setError(
+        console.log(
           err.response?.data?.error ? err.response.data.error : err.message
         );
       });
   }, [user]);
 
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchTerm(e.target.value);
+
+    if (searchTerm.length <= 1) {
+      setSearchResult(tiles);
+    } else {
+      setSearchResult(
+        tiles.filter((item) => {
+          return (
+            item.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            item.size.toLowerCase().includes(searchTerm.toLowerCase())
+          );
+        })
+      );
+    }
+  };
+
   return (
     <>
-      <div className="bg-gray-200 h-[93vh] flex justify-center">
-        <div className="bg-gray-100 inline-flex w-full max-h-[90vh] m-5 p-5 overflow-y-scroll rounded shadow-lg">
-          {/* <h1 className="w-full text-4xl">Tegel overzicht</h1> */}
-          {tiles &&
-          tiles.map((item) => (
-            <TileCard key={item._id} tile={item} />
-          ))}
+      <div className="bg-gray-200 min-h-[93vh] flex justify-center">
+        <div className="bg-gray-100 w-full md:max-w-[1400px] min-h-[90vh] m-5 p-5 rounded shadow-lg">
+          <div className="w-full flex justify-between">
+            <div className="flex gap-2">
+              <h1 className="text-l text-4xl hidden sm:block pl-2 m-2 font-medium">
+                Tegel overzicht
+              </h1>
+              <button
+                type="button"
+                className="bg-gray-800 inline-flex items-center hover:bg-green-800 m-2 py-1 px-3 rounded-md text-white font-medium transition"
+              >
+                <PlusIcon className="size-5 mr-2 text-white" />
+                Nieuwe tegel
+              </button>
+            </div>
+            <input
+              className="min-w-48 m-2 bg-gray-50 border border-gray-300 text-gray-900 text-m rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
+              placeholder="Zoeken..."
+              type="text"
+              onChange={handleSearch}
+              value={searchTerm}
+            />
+          </div>
+          <div className="flex justify-center flex-wrap  overflow-y-scroll ">
+            {searchResult &&
+              searchResult.map((item) => (
+                <TileCard key={item._id} tile={item} />
+              ))}
+            {searchResult &&
+              searchResult.map((item) => (
+                <TileCard key={item._id} tile={item} />
+              ))}
+            {searchResult &&
+              searchResult.map((item) => (
+                <TileCard key={item._id} tile={item} />
+              ))}
+            {searchResult &&
+              searchResult.map((item) => (
+                <TileCard key={item._id} tile={item} />
+              ))}
+            {searchResult &&
+              searchResult.map((item) => (
+                <TileCard key={item._id} tile={item} />
+              ))}
+            {searchResult &&
+              searchResult.map((item) => (
+                <TileCard key={item._id} tile={item} />
+              ))}
+            {searchResult &&
+              searchResult.map((item) => (
+                <TileCard key={item._id} tile={item} />
+              ))}
+          </div>
         </div>
       </div>
     </>
