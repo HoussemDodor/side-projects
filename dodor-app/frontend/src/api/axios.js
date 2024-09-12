2201a95e3226ee5fbdd8e86f0eae7fdf9cb6bb12
Houssem Dodor <houssem@dodor.nl>
2024-09-12 15:11:05 +0200

    removed unused code

--- dodor-app/frontend/src/api/axios.js
+++ dodor-app/frontend/src/api/axios.js
@@ -6,24 +6,5 @@ const api = axios.create({
   baseURL: "http://localhost:3001",
 });
 
-const useAxiosInterceptor = () => {
-  const navigate = useNavigate(); // React Router's navigation function
-  const { logout } = useLogout();
-
-  // Add a response interceptor
-  api.interceptors.response.use(
-    (response) => response, // If the response is successful, just return it
-    (error) => {
-      if (error.response && error.response.status === 401) {
-        // If the response status is 401, log out the user
-        console.log("ERROR FROM /api/axios.js")
-        console.log(error)
-        logout();
-        navigate("/login"); // Redirect to the login page
-      }
-      return Promise.reject(error); // Return the error so it can be caught by other handlers
-    }
-  );
-};
 
 export { api };
